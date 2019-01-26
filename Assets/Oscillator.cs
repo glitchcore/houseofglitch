cdbd80f21c14fd3d4297340164db6eb729c541d9
Anper <mail@s3f.ru>
2019-01-27 02:15:06 +0300

    add audio engine

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    public float freq = 440.0f;
+    public float spread = 0.75f;
+    public float gain = 0.25f;
+
+    private double phase;
+    private double fs = 48000.0;
+    private double increment;
+
+    
+
+    void OnAudioFilterRead(float[] data, int channels) {
+        for(int i = 0; i < data.Length; i += channels) {
+            phase += increment;
+            data[i] = (float) (gain * (
+                Mathf.Sin((float)(phase * freq * 2.0 * Mathf.PI)) +
+                Mathf.Sin((float)(phase * freq * 2.0 * Mathf.PI * spread))
+            ));
+
+            if(channels == 2) {
+                data[i + 1] = data[i];
+            }
+
+            if(phase > (Mathf.PI * 2)) {
+                phase = 0.0;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        increment = 1.0 / fs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
